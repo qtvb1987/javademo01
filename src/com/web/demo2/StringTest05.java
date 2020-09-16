c951a800037b5f95ef776448de3fdac33fd8ff29
qingjiawen <qtvb1987@163.com>
2020-09-16 22:58:39 +0800

    Scanner nextline next

---
+++ src/com/web/demo2/StringTest05.java
@@ -0,0 +1,13 @@
+package com.web.demo2;
+
+import java.util.Scanner;
+
+public class StringTest05 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s1=scanner.nextLine();
+        System.out.println(s1);
+        String s2=scanner.next();
+        System.out.println(s2);
+    }
+}
